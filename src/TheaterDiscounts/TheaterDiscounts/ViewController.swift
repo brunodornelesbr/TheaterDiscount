1f20bb971d90fca57bdeb746bf28aa69bb0cf150
Bruno Dorneles <>
2019-11-25 13:03:14 -0300

    Added view to calculate discount

--- src/TheaterDiscounts/TheaterDiscounts/ViewController.swift
+++ src/TheaterDiscounts/TheaterDiscounts/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var originalTextField: UITextField!
+    @IBOutlet weak var discountTextField: UITextField!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var discountManager : DiscountManagerProtocol!
+    private var currentClient : Client?
     override func viewDidLoad() {
         super.viewDidLoad()
+        discountManager = DiscountManager(currentDate: datePicker.date, discountFinder: DiscountFinder())
+        
         // Do any additional setup after loading the view.
     }
+    
+    func setupScreen(){
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            currentClient = Child()
+        case 1 :
+            currentClient = Elder()
+        case 2 :
+            currentClient = Student(hasCard: false)
+        default:
+            currentClient = Student(hasCard: true)
+        }
+        
+        originalTextField.text = "\(currentClient?.pricePerTicket() ?? 0)"
+        discountTextField.text = "\(discountManager.calculateDiscountTo(client: currentClient!))"
+           
+    }
 
-
+    @IBAction func didChooseAnotherClient(_ sender: UISegmentedControl) {
+        setupScreen()
+    }
+    
+    
+    @IBAction func didChooseDate(_ sender: UIDatePicker) {
+        discountManager = DiscountManager(currentDate: datePicker.date, discountFinder: DiscountFinder())
+        setupScreen()
+    }
+    
 }
 
